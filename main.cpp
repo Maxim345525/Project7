cb28f91b9b7e2fdb389babd0bde21ba89006d355
student <student@PC-C-09>
2023-01-24 20:11:11 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -4,28 +4,31 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    const int size = 10;
+    const int size = 5;
     int arr2D[size][size];
-    int sum = 0;
+    int avg = 0;
     for (size_t i = 0; i < size; i++)
     {
         for (size_t j = 0; j <= size; j++)
         {
-            arr2D[i][j] = 10 + rand() % 10;
+            arr2D[i][j] = 5 + rand() % 5;
             cout << arr2D[i][j] << ' ';
         }
         cout << endl;
     }
 
-    sum = arr2D[0][0];
+    avg = arr2D[0][0];
     for (size_t i = 0; i < size; i++)
     {
         for (size_t j = 0; j < size; j++)
         {
-                sum += arr2D[i][j];
-          
+            avg += arr2D[i][j];
+
+            avg = avg / arr2D[i][j];
         }
+        
     }
-    cout << "Summ-->" << sum;
+
+    cout << "Avg-->" << avg;
     return 0;
 }
